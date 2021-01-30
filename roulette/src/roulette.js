5ec6e12f2fe5f190897848ce94a6f3124de3af54
ystkfujii <ystk.fujii0731@gmail.com>
2021-01-30 20:48:08 +0900

    first commit

---
+++ roulette/src/roulette.js
@@ -0,0 +1,430 @@
+let userData = [
+  ['fujii',  1 ],
+  ['yamada', 1 ],
+  ['fujii',  1 ],
+  ['yamada', 1 ],
+  ['fujii',  1 ],
+  ['yamada', 1 ],
+  ['fujii',  1 ],
+  ['yamada', 1 ],
+  ['fujii',  1 ],
+  ['yamada', 1 ],
+  ['fujii',  1 ],
+  ['sasaki', 2 ]
+];
+
+var _0x54ac = ['sin', 'textAlign', '4px 10px 4px 10px', 'startRouletteCircle', 'placeholder', '4px', 'innerHTML', 'moveRoulette', 'moveRouletteCircle', 'clearRect', 'lineTo', 'sqrt', 'abs', '結果:', 'stroke', 'lineWidth', 'strokeStyle', 'getCookie', 'split', 'saved_roulette', 'saved_title', '読み込み', 'load_btn', 'onclick', 'スタート', 'Back', 'Start', 'Please enter a title', 'Save', 'Result:', 'Read', '你确定要删除它吗？', '结果:', '삭제 하시겠습니까?', '제목을 입력 해주세요', '결과:', 'Seguro que quieres eliminarlo?', 'Mesa', 'Volver', 'Inicio', 'Por favor ingrese un título', 'Guardar', 'Resultado:', 'Leer', 'margin', '0px', 'round', '0123456789ABCDEF', 'charAt', 'floor', 'random', 'Tue, 1-Jan-2030 00:00:00 GMT', 'pathname', 'join', '; expires=', 'cookie', 'indexOf', 'substring', 'checkCookie', 'testName', 'ROULETTE_DATA', 'green', '#003399', 'addEventListener', 'load', 'attachEvent', 'width', 'getElementById', 'roulette_make_table', 'item0', 'ratio0', 'onchange', 'checkTable', 'result_main', 'number', 'roulette_type_fix', 'roulette_type_easy', 'roulette_type_normal', 'make_button_easy', 'make_button_normal', 'make_coin', 'random_easy', 'item_type', 'make_fix', 'make_easy', 'make_normal', 'make_saved', 'fix_template', 'easy_template', 'normal_template', 'saved_template', 'click', 'prototype', 'makeRouletteCoin', 'makeRouletteEasy', 'changeTemplate', 'undefined', 'getLngData', 'loadRoulette', 'displayRoulette', 'rows', 'length', 'value', 'insertRow', 'insertCell', 'createElement', 'type', 'text', 'item', 'size', 'ratio', 'className', 'main_input_txt', 'appendChild', 'style', 'display', 'none', 'block', 'makeRouletteSaved', 'TYPE', 'COUNT', 'DNAME', 'DRATE', 'DCOLR', 'DCOLG', 'DCOLB', 'makeRoulette', 'makeRouletteDelete', '削除してもよろしいですか？', 'splice', 'TITLE', 'setCookie', '2nd', '3rd', '4th', '6th', '7th', '8th', '9th', '10th', '11th', '12th', '13th', '16th', '17th', '18th', '19th', '22nd', '23rd', '25th', '26th', '27th', '28th', '29th', '31st', '32nd', '34th', '35th', '36th', '37th', '38th', '39th', '40th', '41st', '43rd', '44th', '45th', '46th', '48th', '50th', '二十一', '二十二', '二十三', '二十四', '二十六', '二十七', '二十八', '二十九', '三十二', '三十三', '三十四', '三十六', '三十七', '三十八', '三十九', '四十一', '四十二', '四十三', '四十四', '四十七', '四十八', '열하나', '열다섯', '열여섯', '열여덟', '스물하나', '스물둘', '스물넷', '스물다섯', '스물일곱', '스물아홉', '서른하나', '서른둘', '서른셋', '서른넷', '서른여섯', '서른일곱', '서른여덟', '서른아홉', '마흔하나', '마흔둘', '마흔넷', '마흔다섯', '마흔여섯', '마흔일곱', '마흔여덟', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'nueve', 'diez', 'once', 'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve', 'veinte', 'veintiuno', 'veintitres', 'veinticuatro', 'veinticinco', 'veintiséis', 'veintisiete', 'veintiocho', 'veintinueve', 'treinta', 'treinta y dos', 'treinta y tres', 'treinta y cuatro', 'treinta y cinco', 'treinta y seis', 'treinta y siete', 'treinta y ocho', 'treinta y nueve', 'cuarenta y uno', 'cuarenta y dos', 'cuarenta y tres', 'cuarenta y cuatro', 'cuarenta y cinco', 'cuarenta y seis', 'cuarenta y siete', 'cuarenta y ocho', 'cuarenta y nueve', 'cincuenta', 'checked', 'makeRouletteCircle', 'firstChild', '#666666', 'solid', '1px', '#FFFFFF', 'hidden', 'center', '26px', '#000000', '40px', 'CANVAS', 'height', 'getContext', 'absolute', 'rgb(', 'fillRect', 'font', 'bold 18px Arial', 'fillStyle', 'fillText', 'INPUT', 'button', '16px', 'startRoulette', 'タイトルを入力して下さい', '195px', 'saveRoulette', 'removeChild', 'bold', 'beginPath', 'moveTo', 'arc', 'closePath', 'fill', 'cos'];
+(function (_0xb67384, _0x27295a) {
+  var _0x156d7c = function (_0x52a1f8) {
+    while (--_0x52a1f8) {
+      _0xb67384['push'](_0xb67384['shift']());
+    }
+  };
+  _0x156d7c(++_0x27295a);
+}(_0x54ac, 0x167));
+var _0x151c = function (_0x45e4dc, _0x42b91f) {
+  _0x45e4dc = _0x45e4dc - 0x0;
+  var _0x479a49 = _0x54ac[_0x45e4dc];
+  return _0x479a49;
+};
+
+
+var RouletteMakeTableEle, ItemsEle, RatiosEle
+var RouletteMainEle, RouletteStartEle;
+
+
+
+
+var RouletteFlg = 0x0;
+var rouletteData ;
+var RouletteResultHeight = 0x28;
+var RoulettePaddingCircle = 0x18;
+
+var SpinCount = 0x3;
+var SpinCountNext = 0x1;
+
+var TriangleSize = 0x1a;
+
+
+function MainClass() {}
+var mainClass = new MainClass();
+
+
+
+if (window[_0x151c('0x3')]) {
+  window[_0x151c('0x3')](_0x151c('0x4'), MainInit, ![]);
+} else if (window[_0x151c('0x5')]) {
+  window[_0x151c('0x5')]('onload', MainInit);
+} else {
+  window['onload'] = MainInit;
+}
+
+function MainInit() {
+
+  CanvasWidth = screen['height']  * 0x2 / 0x3;
+  CanvasHeight = screen['height'] * 0x2 / 0x3 ;
+  
+  RouletteMakeTableEle = document[_0x151c('0x7')](_0x151c('0x8'));
+  ItemsEle = new Array();
+  ItemsEle[0x0] = document[_0x151c('0x7')](_0x151c('0x9'));
+  RatiosEle = new Array();
+  RatiosEle[0x0] = document['getElementById'](_0x151c('0xa'));
+  
+  ResultMainEle = document[_0x151c('0x7')](_0x151c('0xd'));
+    
+  
+  LNG = 'jp';
+  WLD = mainClass[_0x151c('0x25')](LNG);
+  makeRoulette();
+
+}
+
+
+
+var makeRoulette = function () {
+
+  if (RouletteFlg == 0x2) {
+    clearInterval(RouletteTimer);
+  }
+  var _0x8b30ed = new Array();
+  rouletteData = new Array();
+
+  for (var _0x32a0ee = 0x0; _0x32a0ee < userData.length ; _0x32a0ee++) {
+    var _0x51876b = hsvToRgb(_0x32a0ee * (0x168 / userData.length), 0x41, 0x5f);
+    rouletteData[_0x32a0ee] = [userData[_0x32a0ee][0],userData[_0x32a0ee][1], _0x51876b];
+  }
+  rouletteData = suffle(rouletteData);
+  MainClass[_0x151c('0x20')][_0x151c('0xcb')]();
+};
+
+
+MainClass[_0x151c('0x20')]['checkTable'] = function () {
+  var _0x1a811c = RouletteMakeTableEle[_0x151c('0x28')][_0x151c('0x29')];
+  if (ItemsEle[_0x1a811c - 0x2][_0x151c('0x2a')] && RatiosEle[_0x1a811c - 0x2]['value']) {
+    var _0x34960a = RouletteMakeTableEle[_0x151c('0x2b')](-0x1);
+    var _0x4fa730 = _0x34960a[_0x151c('0x2c')](-0x1);
+    var _0x1f2f59 = _0x34960a[_0x151c('0x2c')](-0x1);
+    ItemsEle[_0x1a811c - 0x1] = document[_0x151c('0x2d')]('INPUT');
+    ItemsEle[_0x1a811c - 0x1][_0x151c('0x2e')] = _0x151c('0x2f');
+    ItemsEle[_0x1a811c - 0x1]['id'] = _0x151c('0x30') + (_0x1a811c - 0x1);
+    ItemsEle[_0x1a811c - 0x1][_0x151c('0x31')] = 0xe;
+    ItemsEle[_0x1a811c - 0x1]['value'] = '';
+    ItemsEle[_0x1a811c - 0x1]['className'] = 'main_input_txt';
+    ItemsEle[_0x1a811c - 0x1][_0x151c('0xb')] = mainClass[_0x151c('0xc')];
+    RatiosEle[_0x1a811c - 0x1] = document[_0x151c('0x2d')]('INPUT');
+    RatiosEle[_0x1a811c - 0x1][_0x151c('0x2e')] = 'text';
+    RatiosEle[_0x1a811c - 0x1]['id'] = _0x151c('0x32') + (_0x1a811c - 0x1);
+    RatiosEle[_0x1a811c - 0x1][_0x151c('0x31')] = 0x6;
+    RatiosEle[_0x1a811c - 0x1]['value'] = '';
+    RatiosEle[_0x1a811c - 0x1][_0x151c('0x33')] = _0x151c('0x34');
+    RatiosEle[_0x1a811c - 0x1][_0x151c('0xb')] = mainClass['checkTable'];
+    _0x4fa730[_0x151c('0x35')](ItemsEle[_0x1a811c - 0x1]);
+    _0x1f2f59[_0x151c('0x35')](RatiosEle[_0x1a811c - 0x1]);
+  }
+};
+
+
+MainClass['prototype'][_0x151c('0xcb')] = function () {
+  if (RouletteFlg == 0x2) {
+    clearInterval(RouletteTimer);
+  }
+  while (ResultMainEle[_0x151c('0xcc')]) ResultMainEle[_0x151c('0xe7')](ResultMainEle[_0x151c('0xcc')]);
+  setStyle(ResultMainEle, {
+    'height': RouletteResultHeight + CanvasHeight  + 'px',
+    'width': screen['width'] + 'px', //CanvasWidth  + 'px',
+    'zIndex': 0x1
+  });
+  
+  RouletteMainEle = createDiv();
+  setStyle(RouletteMainEle, {
+    'borderColor': _0x151c('0xcd'),
+    'borderStyle': 'solid',
+    'borderWidth': _0x151c('0xcf'),
+    'backgroundColor': _0x151c('0xd0'),
+    'width': CanvasWidth + 'px',
+    'height': RouletteResultHeight + CanvasHeight  + 'px',
+    'position': _0x151c('0xd9'),
+    'overflow': _0x151c('0xd1'),
+    'zIndex': 0x1
+  });
+  
+  ResultMainEle['appendChild'](RouletteMainEle);
+  RouletteResultEle = createDiv();
+  setStyle(RouletteResultEle, {
+    'backgroundColor': _0x151c('0xd0'),
+    'width': CanvasWidth + 'px',
+    'height': RouletteResultHeight + 'px',
+    'position': _0x151c('0xd9'),
+    'textAlign': _0x151c('0xd2'),
+    'fontSize': _0x151c('0xd3'),
+    'color': _0x151c('0xd4'),
+    'fontWeight': _0x151c('0xe8'),
+    'lineHeight': _0x151c('0xd5'),
+    'zIndex': 0x1
+  });
+  
+  RouletteMainEle[_0x151c('0x35')](RouletteResultEle);
+  RouletteMainCanvas = document['createElement'](_0x151c('0xd6'));
+  setStyle(RouletteMainCanvas, {
+    'width': CanvasWidth + 'px',
+    'height': CanvasHeight * 'px',
+    'top': RouletteResultHeight + 'px',
+    'position': _0x151c('0xd9'),
+    'zIndex': 0x1
+  });
+  
+  RouletteMainCanvas[_0x151c('0x6')] = CanvasWidth;
+  RouletteMainCanvas[_0x151c('0xd7')] = CanvasHeight;
+  RouletteMainEle['appendChild'](RouletteMainCanvas);
+  ctx = RouletteMainCanvas[_0x151c('0xd8')]('2d');
+  RouletteMainCanvas2 = document[_0x151c('0x2d')]('CANVAS');
+  setStyle(RouletteMainCanvas2, {
+    'width': CanvasWidth + 'px',
+    'height': CanvasHeight * 'px',
+    'top': RouletteResultHeight + 'px',
+    'position': _0x151c('0xd9'),
+    'zIndex': 0x1
+  });
+  
+  RouletteMainCanvas2[_0x151c('0x6')] = CanvasWidth;
+  RouletteMainCanvas2[_0x151c('0xd7')] = CanvasHeight;
+  RouletteMainEle[_0x151c('0x35')](RouletteMainCanvas2);
+  
+  ctx2 = RouletteMainCanvas2[_0x151c('0xd8')]('2d');
+  rouletteTotalRate = 0x0;
+  for (var _0x5d360b = 0x0; _0x5d360b < rouletteData['length']; _0x5d360b++) {
+    rouletteTotalRate += rouletteData[_0x5d360b][0x1];
+  }
+  var _0x41accc = (CanvasWidth - RoulettePaddingCircle * 0x2) / 0x2;
+  var _0x5e28d9 = -Math['PI'] / 0x2;
+  var _0x441199 = 0x0;
+  var _0x51ff03 = 0x0;
+  for (var _0x5d360b = 0x0; _0x5d360b < rouletteData['length']; _0x5d360b++) {
+    ctx[_0x151c('0xde')] = _0x151c('0xda') + rouletteData[_0x5d360b][0x2]['r'] + ', ' + rouletteData[_0x5d360b][0x2]['g'] + ', ' + rouletteData[_0x5d360b][0x2]['b'] + ')';
+    _0x441199 = _0x5e28d9 + Math['PI'] * 0x2 * (rouletteData[_0x5d360b][0x1] / rouletteTotalRate);
+    ctx[_0x151c('0xe9')]();
+    ctx[_0x151c('0xea')](CanvasWidth / 0x2, CanvasHeight / 0x2);
+    ctx[_0x151c('0xeb')](CanvasWidth / 0x2, CanvasHeight / 0x2, _0x41accc, _0x5e28d9, _0x441199, ![]);
+    ctx[_0x151c('0xec')]();
+    ctx[_0x151c('0xed')]();
+    _0x5e28d9 = _0x441199;
+    if (rouletteData[_0x5d360b][0x1][_0x151c('0x29')] > 0x3) {
+      _0x51ff03 = 0x1;
+    }
+  }
+  _0x5e28d9 = -Math['PI'] / 0x2;
+  _0x441199 = 0x0;
+  _0x41accc = _0x41accc * 0x2 / 0x3;
+  var _0x45a36a = CanvasWidth / 0x2;
+  var _0x423dbd = CanvasHeight / 0x2;
+  for (var _0x5d360b = 0x0; _0x5d360b < rouletteData[_0x151c('0x29')]; _0x5d360b++) {
+    _0x441199 = _0x5e28d9 + Math['PI'] * 0x2 * (rouletteData[_0x5d360b][0x1] / rouletteTotalRate);
+    var _0x47b135 = (_0x441199 - _0x5e28d9) / 0x2;
+    var _0x18f1ee;
+    if (_0x51ff03) {
+      _0x18f1ee = _0x5e28d9;
+      if (_0x18f1ee < Math['PI'] / 0x2) {
+        _0x18f1ee = 0x1;
+      } else {
+        _0x18f1ee = 1.3;
+      }
+    } else {
+      _0x18f1ee = 0x1;
+    }
+    var _0x316006 = _0x18f1ee * _0x41accc * Math[_0x151c('0xee')](_0x5e28d9 + _0x47b135);
+    var _0x13028f = _0x18f1ee * _0x41accc * Math[_0x151c('0xef')](_0x5e28d9 + _0x47b135);
+    ctx2[_0x151c('0xf0')] = 'center';
+    ctx2[_0x151c('0xdc')] = _0x151c('0xdd');
+    ctx2['fillStyle'] = '#000000';
+    ctx2[_0x151c('0xdf')](rouletteData[_0x5d360b][0x0], _0x45a36a + _0x316006, _0x423dbd + _0x13028f, _0x41accc * 1.1);
+    ctx2[_0x151c('0xf0')] = 'start';
+    _0x5e28d9 = _0x441199;
+  }
+  
+  RouletteFlg = 0x1;
+};
+
+
+MainClass['prototype']['remake'] = function () {
+  makeRoulette();
+};
+
+
+MainClass['prototype']['startRouletteCircle'] = function () {
+  if (RouletteFlg != 0x1 && RouletteFlg != 0x3) {
+    return;
+  } else {
+    RouletteFlg = 0x2;
+  }
+  RouletteResultEle['innerHTML'] = '';
+  var _0x402364 = getRand(rouletteTotalRate);
+  var _0x50180b = 0x0;
+  RouletteResult = 0x0;
+  for (var _0x256fe = 0x0; _0x256fe < rouletteData[_0x151c('0x29')]; _0x256fe++) {
+    _0x50180b += rouletteData[_0x256fe][0x1];
+    if (_0x402364 <= _0x50180b) {
+      RouletteResult = _0x256fe;
+      break;
+    }
+  }
+  var _0x3ee19b = (CanvasWidth - RoulettePaddingCircle * 0x2) / 0x2 + 0x1;
+  var _0x151ef7 = _0x3ee19b * 0x2 * Math['PI'] * 0x2 * (rouletteData[RouletteResult][0x1] / rouletteTotalRate);
+  var _0x2ceec0 = 0x0;
+  for (var _0x256fe = 0x0; _0x256fe < rouletteData[_0x151c('0x29')]; _0x256fe++) {
+    if (RouletteResult > _0x256fe) {
+      _0x2ceec0 += _0x3ee19b * 0x2 * Math['PI'] * 0x2 * (rouletteData[_0x256fe][0x1] / rouletteTotalRate);
+    } else {
+      break;
+    }
+  }
+  RouletteCount = 0x0;
+  RouletteCountMax = _0x3ee19b * 0x2 * Math['PI'] * 0x2 * (SpinCount + SpinCountNext) + _0x2ceec0 + getRand(_0x151ef7);
+  RouletteTimer = setInterval(MainClass[_0x151c('0x20')][_0x151c('0xf7')], 0x15);
+};
+
+
+MainClass['prototype'][_0x151c('0xf7')] = function () {
+  var _0x4c538a = CanvasWidth / 0x2;
+  var _0x2ab7bd = CanvasHeight / 0x2;
+  var _0x3ede33 = (CanvasWidth - RoulettePaddingCircle * 0x2) / 0x2;
+  var _0x318a73 = _0x3ede33 + 0x1;
+  
+  ctx[_0x151c('0xe9')]();
+  ctx['lineWidth'] = 0x1c;
+  ctx[_0x151c('0xeb')](_0x4c538a, _0x2ab7bd, _0x3ede33 + 0xf, 0x0, Math['PI'] * 0x2, ![]);
+  ctx['strokeStyle'] = _0x151c('0xd0');
+  ctx[_0x151c('0xfd')]();
+  ctx[_0x151c('0xec')]();
+  ctx['fillStyle'] = _0x151c('0xd4');
+  ctx[_0x151c('0xe9')]();
+  var _0x5a35ca = Math['PI'] * 0x2 * (RouletteCount % (_0x318a73 * 0x2 * Math['PI'] * 0x2) / (_0x318a73 * 0x2 * Math['PI'] * 0x2)) - Math['PI'] / 0x2;
+  var _0x3e1bee = _0x5a35ca - Math['PI'] / 0x3;
+  var _0x5fb4a7 = _0x5a35ca;
+  var _0x582d5c = _0x4c538a + _0x318a73 * Math[_0x151c('0xee')](_0x5a35ca);
+  var _0x4c110a = _0x2ab7bd + _0x318a73 * Math[_0x151c('0xef')](_0x5a35ca);
+  var _0x50eea6 = Math[_0x151c('0xee')](_0x3e1bee);
+  var _0x46d213 = Math['sin'](_0x3e1bee);
+  var _0x10b7f2 = Math[_0x151c('0xee')](_0x5fb4a7);
+  var _0x6c7774 = Math[_0x151c('0xef')](_0x5fb4a7);
+  var _0x4d5297 = TriangleSize;
+  var _0x5ef85c = TriangleSize / Math['sqrt'](0x3);
+  ctx[_0x151c('0xea')](_0x582d5c, _0x4c110a);
+  ctx[_0x151c('0xf9')](_0x582d5c + _0x4d5297 * _0x50eea6 - _0x5ef85c * _0x46d213, _0x4c110a + _0x4d5297 * _0x46d213 + _0x5ef85c * _0x50eea6);
+  ctx[_0x151c('0xf9')](_0x582d5c + _0x4d5297 * _0x10b7f2 - _0x5ef85c * _0x6c7774, _0x4c110a + _0x4d5297 * _0x6c7774 + _0x5ef85c * _0x10b7f2);
+  ctx[_0x151c('0xec')]();
+  ctx[_0x151c('0xed')]();
+  if (RouletteCount < _0x3ede33 * 0x2 * Math['PI'] * 0x2 * SpinCount) {
+    RouletteCount += 0x32;
+  } else {
+    var _0x54199d = RouletteCountMax - _0x3ede33 * 0x2 * Math['PI'] * 0x2 * SpinCount;
+    var _0x4df146 = RouletteCountMax - RouletteCount;
+    RouletteCount += 49.5 * (_0x4df146 / _0x54199d) + 0.6;
+  }
+  if (Math[_0x151c('0xfb')](RouletteCountMax - RouletteCount) < 0x1) {
+    ctx[_0x151c('0xe9')]();
+    ctx[_0x151c('0xfe')] = 0x1c;
+    ctx['arc'](_0x4c538a, _0x2ab7bd, _0x3ede33 + 0xe, 0x0, Math['PI'] * 0x2, ![]);
+    ctx[_0x151c('0xff')] = _0x151c('0xd0');
+    ctx[_0x151c('0xfd')]();
+    ctx[_0x151c('0xec')]();
+    ctx[_0x151c('0xde')] = _0x151c('0xd4');
+    ctx[_0x151c('0xe9')]();
+    _0x5a35ca = Math['PI'] * 0x2 * (RouletteCountMax % (_0x318a73 * 0x2 * Math['PI'] * 0x2) / (_0x318a73 * 0x2 * Math['PI'] * 0x2)) - Math['PI'] / 0x2;
+    _0x3e1bee = _0x5a35ca - Math['PI'] / 0x3;
+    _0x5fb4a7 = _0x5a35ca;
+    _0x582d5c = _0x4c538a + _0x318a73 * Math[_0x151c('0xee')](_0x5a35ca);
+    _0x4c110a = _0x2ab7bd + _0x318a73 * Math[_0x151c('0xef')](_0x5a35ca);
+    _0x50eea6 = Math[_0x151c('0xee')](_0x3e1bee);
+    _0x46d213 = Math[_0x151c('0xef')](_0x3e1bee);
+    _0x10b7f2 = Math['cos'](_0x5fb4a7);
+    _0x6c7774 = Math[_0x151c('0xef')](_0x5fb4a7);
+    _0x4d5297 = TriangleSize;
+    _0x5ef85c = TriangleSize / Math['sqrt'](0x3);
+    ctx[_0x151c('0xea')](_0x582d5c, _0x4c110a);
+    ctx[_0x151c('0xf9')](_0x582d5c + _0x4d5297 * _0x50eea6 - _0x5ef85c * _0x46d213, _0x4c110a + _0x4d5297 * _0x46d213 + _0x5ef85c * _0x50eea6);
+    ctx[_0x151c('0xf9')](_0x582d5c + _0x4d5297 * _0x10b7f2 - _0x5ef85c * _0x6c7774, _0x4c110a + _0x4d5297 * _0x6c7774 + _0x5ef85c * _0x10b7f2);
+    ctx[_0x151c('0xec')]();
+    ctx[_0x151c('0xed')]();
+    clearInterval(RouletteTimer);
+    RouletteFlg = 0x3;
+    RouletteResultEle[_0x151c('0xf5')] = WLD['結果:'] + rouletteData[RouletteResult][0x0];
+  }
+};
+MainClass[_0x151c('0x20')][_0x151c('0x25')] = function (_0x1a6e23) {
+  var _0x39b26a;
+  _0x39b26a = {
+      'スタート': _0x151c('0x107'),
+      '結果:': '結果:',
+      '\xD7': '×'
+  };
+  return _0x39b26a;
+};
+var createDiv = function () {
+  var _0x3d6472 = document['createElement']('DIV');
+  _0x3d6472[_0x151c('0x36')][_0x151c('0x11b')] = _0x151c('0x11c');
+  _0x3d6472[_0x151c('0x36')]['padding'] = _0x151c('0x11c');
+  return _0x3d6472;
+};
+var setStyle = function (_0x4ad688, _0x2d1fa8) {
+  for (var _0x5982e2 in _0x2d1fa8) {
+    _0x4ad688['style'][_0x5982e2] = _0x2d1fa8[_0x5982e2];
+  }
+};
+var getRand = function (_0x8b0154) {
+  return Math['random']() * _0x8b0154;
+};
+var hsvToRgb = function (_0x58b912, _0x4d8242, _0x2fa590) {
+  var _0x4a9201 = {
+    'h': _0x58b912,
+    's': _0x4d8242,
+    'v': _0x2fa590
+  };
+  var _0x1e3715 = {
+    'r': 0x0,
+    'g': 0x0,
+    'b': 0x0
+  };
+  var _0x49449b = _0x4a9201['v'] / 0x64 * 0xff;
+  var _0x4eb7ec = _0x49449b - _0x4a9201['s'] / 0x64 * _0x49449b;
+  if (0x0 <= _0x4a9201['h'] && _0x4a9201['h'] < 0x3c) {
+    _0x1e3715['r'] = _0x49449b;
+    _0x1e3715['g'] = _0x4a9201['h'] / 0x3c * (_0x49449b - _0x4eb7ec) + _0x4eb7ec;
+    _0x1e3715['b'] = _0x4eb7ec;
+  } else if (0x3c <= _0x4a9201['h'] && _0x4a9201['h'] < 0x78) {
+    _0x1e3715['r'] = (0x78 - _0x4a9201['h']) / 0x3c * (_0x49449b - _0x4eb7ec) + _0x4eb7ec;
+    _0x1e3715['g'] = _0x49449b;
+    _0x1e3715['b'] = _0x4eb7ec;
+  } else if (0x78 <= _0x4a9201['h'] && _0x4a9201['h'] < 0xb4) {
+    _0x1e3715['r'] = _0x4eb7ec;
+    _0x1e3715['g'] = _0x49449b;
+    _0x1e3715['b'] = (_0x4a9201['h'] - 0x78) / 0x3c * (_0x49449b - _0x4eb7ec) + _0x4eb7ec;
+  } else if (0xb4 <= _0x4a9201['h'] && _0x4a9201['h'] < 0xf0) {
+    _0x1e3715['r'] = _0x4eb7ec;
+    _0x1e3715['g'] = (0xf0 - _0x4a9201['h']) / 0x3c * (_0x49449b - _0x4eb7ec) + _0x4eb7ec;
+    _0x1e3715['b'] = _0x49449b;
+  } else if (0xf0 <= _0x4a9201['h'] && _0x4a9201['h'] < 0x12c) {
+    _0x1e3715['r'] = (_0x4a9201['h'] - 0xf0) / 0x3c * (_0x49449b - _0x4eb7ec) + _0x4eb7ec;
+    _0x1e3715['g'] = _0x4eb7ec;
+    _0x1e3715['b'] = _0x49449b;
+  } else if (0x12c <= _0x4a9201['h'] && _0x4a9201['h'] <= 0x168) {
+    _0x1e3715['r'] = _0x49449b;
+    _0x1e3715['g'] = _0x4eb7ec;
+    _0x1e3715['b'] = (0x168 - _0x4a9201['h']) / 0x3c * (_0x49449b - _0x4eb7ec) + _0x4eb7ec;
+  }
+  _0x1e3715['r'] = Math[_0x151c('0x11d')](_0x1e3715['r']);
+  _0x1e3715['g'] = Math[_0x151c('0x11d')](_0x1e3715['g']);
+  _0x1e3715['b'] = Math['round'](_0x1e3715['b']);
+  return _0x1e3715;
+};
+
+var suffle = function (_0xb82711) {
+  var _0x57afd2 = _0xb82711[_0x151c('0x29')],
+    _0x4d8202, _0xf4e973;
+  while (_0x57afd2) {
+    _0xf4e973 = Math['floor'](Math[_0x151c('0x121')]() * _0x57afd2--);
+    _0x4d8202 = _0xb82711[_0x57afd2];
+    _0xb82711[_0x57afd2] = _0xb82711[_0xf4e973];
+    _0xb82711[_0xf4e973] = _0x4d8202;
+  }
+  return _0xb82711;
+};
